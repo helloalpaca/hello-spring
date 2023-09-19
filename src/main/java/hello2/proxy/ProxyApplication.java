f2f06a6413a723d52f683a7c50d6173dc92d9c6e
helloalpaca <jms393497@gmail.com>
2023-09-19 11:29:08 +0900

    Feat: 스프링 고급편 - 동적 프록시 까지

--- src/main/java/hello2/proxy/ProxyApplication.java
+++ src/main/java/hello2/proxy/ProxyApplication.java
@@ -1,6 +1,6 @@
 package hello2.proxy;
 
-import hello2.proxy.config.v1_proxy.ConcreteProxyConfig;
+import hello2.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello2.proxy.trace.logtrace.LogTrace;
 import hello2.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -11,7 +11,9 @@ import org.springframework.context.annotation.Import;
 //@Import(AppV1Config.class)
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import(InterfaceProxyConfig.class)
-@Import(ConcreteProxyConfig.class)
+//@Import(ConcreteProxyConfig.class)
+//@Import(DynamicProxyBasicConfig.class)
+@Import(DynamicProxyFilterConfig.class)
 @SpringBootApplication(scanBasePackages = "hello2.proxy.app")
 public class ProxyApplication {
 
